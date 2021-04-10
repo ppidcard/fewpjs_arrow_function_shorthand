bf1fff81eb1fcb6df22dfb42b829fa5ba4b4656a
ppidcard <tvxqlikei@live.cn>
2021-04-11 01:38:41 +1000

    Done with assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+const divide = function(){
+    return 2000/100;
+}
+const square = x => x * x;
+
+const add = (a,b) => a + b;
+
+/*
+
+
+  it("has an arrow function called add", () => {
+    expect(add).to.exist
+  })
+
+  it("add arrow function takes two parameters and adds them together", () => {
+    let a = 3
+    let b = 4
+    expect(add(a,b)).to.equal(a+b)
+  })
+})
+*/
